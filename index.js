5639a7267959628fdb065b5e75275ade34561496
siva9996 <rmsiva9996@gmail.com>
2024-02-26 13:05:26 +0530

    foodbackend

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config();
 const router = require('./Routes/Createuser')
 
 app.use((req, res, next) => {
     console.log(req.path + ' ' + req.method)
     next()
 })
-app.use(cors())
+const corsConfig = {
+    origin: "*",
+    credential: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+}
+app.options("", cors(corsConfig))
+app.use(cors(corsConfig))
+
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 app.use(express.static('public/'))
 app.use(express.json())
-mongoose.connect('mongodb+srv://rmsiva9996:LzwSkUHEP5FGP6BB@cluster0.ss1xdqe.mongodb.net/').then(() => {
-    app.listen(5000, () => {
+mongoose.connect(process.env.MONGODB).then(() => {
+    app.listen(process.env.PORT, () => {
         console.log('connection successful')
     })
 }).catch((er) => {
